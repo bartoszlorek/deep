f78575cdaaa610a73354b388a293fc8ba5d1eb1e
Bartosz Lorek <bery.lorek@gmail.com>
2017-10-15 13:33:57 +0200

    incorrect module.exports

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import deepEach from '../src/deep-each'
 import deepMap from '../src/deep-map'
 import deepFilter from '../src/deep-filter'
 
-module.export = {
+module.exports = {
     deepEach,
     deepMap,
     deepFilter
